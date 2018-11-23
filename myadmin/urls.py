9fee73835e65e85f9f3ae7424509bd55c9a55e18
Unknown <1445660110@qq.com>
2018-11-23 21:22:36 +0800

    放入源码

---
+++ myadmin/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from. import views
+
+app_name='myadmin'
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('edit/<forloop_counter>', views.edit, name='edit'),
+    path('del/<forloop_counter>', views.delete, name='del'),
+    path('cross/<forloop_counter>', views.cross, name='cross'),
+
+]
+
---
+++ myadmin/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render, redirect
+
+# Create your views here.
+to_do_list = [
+		{'task': '任务一', 'process' : False},
+		{'task': '任务二', 'process': True},
+	]
+
+
+def home(request):
+	to_do = {'to_do_list': to_do_list}
+	if request.method == 'POST':
+		if request.POST['to_do'].strip() =='':
+			to_do['errno'] = '警告！ 您输入的为空值'
+			return render(request, 'myadmin/home.html', to_do)
+		else:
+			to_do['errno'] = '添加成功'
+			to_do['to_do_list'].append({'task':request.POST['to_do'], 'process': False})
+			print(to_do)
+			return render(request, 'myadmin/home.html', to_do)
+	elif request.method == 'GET':
+		return render(request, 'myadmin/home.html', to_do)		
+
+def about(request):
+	return render(request, 'myadmin/about.html')
+
+def edit(request, forloop_counter):
+	to_do = {'to_do_list': to_do_list}
+	if request.method == 'POST':
+		if request.POST['edit_task'].strip() == '':
+			to_do['errno'] = '警告！ 您输入的为空值'
+			return render(request, 'myadmin/edit.html', to_do)
+		else:
+			to_do['errno'] = '添加成功'
+			to_do_list[int(forloop_counter) - 1]['task'] = request.POST['edit_task']
+			print(to_do)
+			return redirect('myadmin:home', to_do)	
+	else:
+		content = { 'edit_task': to_do_list[int(forloop_counter) - 1]['task']} 
+		return render(request, 'myadmin/edit.html', content)	
+
+def delete(request, forloop_counter):
+	to_do_list.pop(int(forloop_counter) - 1)
+	return redirect('myadmin:home')
+
+def cross(requset, forloop_counter):
+	print(forloop_counter)
+	print(requset.POST['process'])
+	if int(requset.POST['process']) == 1:
+		to_do_list[int(forloop_counter) - 1]['process'] = False
+		return redirect('myadmin:home')
+	else:
+		to_do_list[int(forloop_counter) - 1]['process'] = True
+		return redirect('myadmin:home')
